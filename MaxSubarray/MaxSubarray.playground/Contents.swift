74b0e88f37e620a34dc561e3ff88aae452e34000
Alex Tsyss <aetsyss@mail.ru>
2020-01-03 21:07:15 +0300

    maximum subarray

--- MaxSubarray/MaxSubarray.playground/Contents.swift
+++ MaxSubarray/MaxSubarray.playground/Contents.swift
@@ -7,7 +7,7 @@ func maxSubArray(_ nums: [Int]) -> Int {
     var local_max = nums[0]
     var global_max = nums[0]
 
-    for n in nums {
+    for n in nums.dropFirst() {
         local_max = max(local_max + n, n)
         global_max = max(global_max, local_max)
     }
